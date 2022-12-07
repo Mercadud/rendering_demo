7b181bcade84f5afd78e6d1dfeebbf0b2c330e1a
Marc Daaboul <daaboulmarc@gmail.com>
2022-12-06 23:22:01 -0500

    weee

--- src/shaders/mod.rs
+++ src/shaders/mod.rs
@@ -1,9 +1,12 @@
+use vulkano::impl_vertex;
+use bytemuck::{Pod, Zeroable};
+
 pub mod vs {
     vulkano_shaders::shader! {
         ty: "vertex",
         path: "src/shaders/shader.vert",
         types_meta: {
-            use bytemuck::{Pod, Zeroable};
+            use bytemuck::{Zeroable, Pod};
 
             #[derive(Clone, Copy, Zeroable, Pod)]
         },
@@ -15,9 +18,25 @@ pub mod fs {
         ty: "fragment",
         path: "src/shaders/shader.frag",
         types_meta: {
-            use bytemuck::{Pod, Zeroable};
-
             #[derive(Clone, Copy, Zeroable, Pod)]
         }
     }
 }
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, Default, Zeroable, Pod)]
+pub struct Vertex {
+    pub position: [f32; 3],
+    pub normal: [f32; 3],
+}
+
+impl_vertex!(Vertex, position, normal);
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, Default, Zeroable, Pod)]
+pub struct MonkeInstance {
+    pub colour: [f32; 3],
+    pub object_matrix: [[f32; 4]; 4]
+}
+
+impl_vertex!(MonkeInstance, colour, object_matrix);
--- src/shaders/shader.frag
+++ src/shaders/shader.frag
@@ -1,25 +1,21 @@
 #version 450
 
 layout (location = 0) in vec3 v_normal;
-layout (location = 1) in vec3 v_colour;
+//layout (location = 1) in vec3 v_colour;
 
 layout (location = 0) out vec4 f_color;
 
-const vec3 LIGHT = vec3(0.0, 0.0, 1.0);
+const vec3 LIGHT = vec3(0.0, 0.0, 100.0);
 
 
-layout (set = 0, binding = 1) uniform Data {
-    bool lighting;
-} uniforms;
+//layout (set = 0, binding = 1) uniform Data {
+//    bool lighting;
+//} uniforms;
 
 void main() {
-    if (uniforms.lighting == true) {
         float brightness = dot(normalize(v_normal), normalize(LIGHT));
         vec3 dark_color = vec3(0.0, 0.0, 0.0);
-        vec3 regular_color = v_colour;
+        vec3 regular_color = vec3(0.0, 1.0, 0.0);
 
         f_color = vec4(mix(dark_color, regular_color, brightness), 1.0);
-    } else {
-        f_color = vec4(v_colour, 1.0);
-    }
 }
